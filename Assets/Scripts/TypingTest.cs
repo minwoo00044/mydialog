8ac53ed27d5039675016065c9f10b593c53df5b3
minwoo00044 <minwoo0004@gmail.com>
2023-10-05 01:04:09 +0900

    10-05 homework

--- Assets/Scripts/TypingTest.cs
+++ Assets/Scripts/TypingTest.cs
@@ -13,54 +13,82 @@ public class TypingTest : MonoBehaviour
     private int nextIndex;
     private float txtSpeed;
     private WaitForSeconds waitForSeconds;
-    private Sequence _sequence;
+    private Tweener _currentTween;
+    const float ORIGIN_SPEED = 4F;
 
+    IEnumerator _currentCoroutine;
     private void Start()
     {
         DOTween.Init();
-        txtSpeed = 2f;
+        txtSpeed = ORIGIN_SPEED;
         waitForSeconds = new WaitForSeconds(txtSpeed + 0.5f);
-        _sequence = DOTween.Sequence();
     }
     public void StartDialog()
     {
-        if (_sequence.active)
-            return;
-        if (_sequence != null) 
-        {
-            SequenceInit();
-        }
-        StartCoroutine(DialogRead(dialogs));
+        _currentCoroutine = DialogRead(dialogs);
+        StartCoroutine(_currentCoroutine);
     }
     IEnumerator DialogRead(string[] data)
     {
-        _sequence.Append(text.DOText(data[nextIndex], txtSpeed));
-        _sequence.Play();
+        _currentTween = text.DOText(data[nextIndex], txtSpeed);
         yield return waitForSeconds;
         NextDialog();
     }
     private void NextDialog()
     {
         nextIndex++;
-        text.text = null;
-        SequenceInit();
-        if (nextIndex > dialogs.Length)
+        text.text = string.Empty;
+        if (nextIndex >= dialogs.Length)
         {
             nextIndex = 0;
             return;
         }
-        StartCoroutine(DialogRead(dialogs));
-    }
-    private void SequenceInit()
-    {
-        _sequence.Kill();
-        _sequence = DOTween.Sequence();
+        StartCoroutine(_currentCoroutine);
     }
 
     public void ChangeSpeed()
     {
-        _sequence.timeScale = txtSpeedSlider.value;
-        _sequence.Restart();
-        Debug.Log($"타임스케일{_sequence.timeScale}, 밸류 {txtSpeedSlider.value}");
+        txtSpeed = ORIGIN_SPEED - txtSpeedSlider.value;
+        if (_currentTween != null && _currentTween.IsActive() && !_currentTween.IsComplete())
+        {
+            string currentText = text.text; // 현재 텍스트 스냅샷
+            _currentTween?.Kill(); // 현재 트윈 종료
+
+            float remainingRatio = (float)(dialogs[nextIndex].Length - currentText.Length) / dialogs[nextIndex].Length;
+            // 남아 있는 문자열 비율 계산
+
+            _currentTween = text.DOText(dialogs[nextIndex].Substring(currentText.Length), txtSpeed * remainingRatio)
+                .SetRelative()
+                .SetDelay(0.1f);
+            // 새롭게 남은 문자열부터 다시 시작, SetRelative를 사용해 기존의 문자열에 추가되도록 설정
+        }
+    }
+
+
+    public void SkipAndNextDIalog()
+    {
+        if (nextIndex == dialogs.Length - 1)
+            return;
+        _currentTween?.Kill();
+        text.text = string .Empty;
+        nextIndex++;
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
+
+        _currentCoroutine = DialogRead(dialogs);
+        StartCoroutine(_currentCoroutine);
+    }
+    public void SkipAndPrevDIalog()
+    {
+        if (nextIndex == 0)
+            return;
+        _currentTween?.Kill();
+        text.text = string.Empty;
+        nextIndex--;
+        if (_currentCoroutine != null)
+            StopCoroutine(_currentCoroutine);
+
+        _currentCoroutine = DialogRead(dialogs);
+        StartCoroutine(_currentCoroutine);
     }
 }
